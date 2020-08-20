79586080d2456064ab68fbc65a4361fe60686083
zxl <412750135@qq.com>
2020-08-20 16:10:49 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
 var baseURL = 'http://ajax.frontend.itheima.net';
 $.ajaxPrefilter(function (params) {
     params.url = baseURL + params.url;
+    // 对需要权限的接口配置头信息 以/my/开头的
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+
+    // 拦截所有响应，判断身份认证信息
+    params.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            // 1.清空本地token
+            localStorage.removeItem('token');
+            // 2.页面跳转
+            location.href = '/login.html';
+        }
+    }
 });
